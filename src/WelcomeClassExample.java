9b5125159c55e05672f550bba76a36c8336a9322
marulasiddaiah.mr@ahana.co.in <marulasiddaiah.mr@ahana.co.in>
2021-11-08 17:45:14 +0530

    first commit

---
+++ src/WelcomeClassExample.java
@@ -0,0 +1,12 @@
+
+public class WelcomeClassExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=10; i++) {
+			System.out.println("Welcome to Ahana Systems and Solutions....."+i);
+		}
+
+	}
+
+}
