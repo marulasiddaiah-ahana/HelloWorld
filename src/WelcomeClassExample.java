8e0b486498ed7a12889123822998afd639a40ffa
marulasiddaiah.mr@ahana.co.in <marulasiddaiah.mr@ahana.co.in>
2021-11-08 23:29:40 +0530

    8 times iteraiton

--- src/WelcomeClassExample.java
+++ src/WelcomeClassExample.java
@@ -3,7 +3,7 @@ public class WelcomeClassExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1; i<=5; i++) {
+		for(int i=1; i<=8; i++) {
 			System.out.println("Welcome to Ahana Systems and Solutions....."+i);
 		}
 
